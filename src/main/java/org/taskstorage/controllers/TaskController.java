bffc410eb9bfe1eeca2b083ddf2d278a9399d70a
Dmitrii Gorislavetc <goris2006@gmail.com>
2024-02-25 20:13:12 +0100

    cleanup

--- src/main/java/org/taskstorage/controllers/TaskController.java
+++ src/main/java/org/taskstorage/controllers/TaskController.java
@@ -8,6 +8,7 @@ import org.taskstorage.common.TaskPriority;
 import org.taskstorage.dtos.TaskCreateRequestDTO;
 import org.taskstorage.dtos.TaskDTO;
 import org.taskstorage.dtos.TaskUpdateRequestDTO;
+import org.taskstorage.exceptions.TaskNotFoundException;
 import org.taskstorage.service.TaskStorageService;
 
 import java.util.List;
@@ -71,7 +72,7 @@ public class TaskController {
     public ResponseEntity<Object> getTaskById(@PathVariable("taskId") long taskId) {
         try {
             return ResponseEntity.ok(taskStorageService.getTaskById(taskId));
-        } catch (RuntimeException e) {
+        } catch (TaskNotFoundException e) {
             return ResponseEntity.status(HttpStatus.NOT_FOUND).body(e.getMessage());
         }
     }
@@ -80,7 +81,7 @@ public class TaskController {
     public ResponseEntity<Object> createTask(@RequestBody TaskCreateRequestDTO createRequest) {
         try {
             return ResponseEntity.ok(taskStorageService.createTask(createRequest));
-        } catch (RuntimeException e) {
+        } catch (TaskNotFoundException e) {
             return ResponseEntity.status(HttpStatus.NOT_FOUND).body(e.getMessage());
         }
     }
@@ -92,7 +93,7 @@ public class TaskController {
     ) {
         try {
             return ResponseEntity.ok(taskStorageService.updateTask(taskId, updateRequest));
-        } catch (RuntimeException e) {
+        } catch (TaskNotFoundException e) {
             return ResponseEntity.status(HttpStatus.NOT_FOUND).body(e.getMessage());
         }
     }
@@ -102,7 +103,7 @@ public class TaskController {
         try {
             taskStorageService.deleteTask(taskId);
             return ResponseEntity.ok().build();
-        } catch (RuntimeException e) {
+        } catch (TaskNotFoundException e) {
             return ResponseEntity.status(HttpStatus.NOT_FOUND).body(e.getMessage());
         }
     }
